a869bdde3be34e8205e1f18bec0310329a208649
jwojtowi <joanna.wojtowicz@outlook.com>
2021-08-01 15:48:29 +0200

    Pierwsz yprogram Hello + tablica args.

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,15 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
+using System;
 
 namespace Hello
 {
     class Program
     {
         static void Main(string[] args)
+        
         {
+
+            string name = args[0]; 
+            Console.WriteLine("Witaj"+ name);
+
+            //foreach (var item in args)
+            //{
+            //    Console.WriteLine("Witaj, " + item);
+            //}
+            
         }
     }
 }
